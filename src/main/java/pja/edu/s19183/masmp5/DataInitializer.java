47e4fe0f9356fe26160097642925c11d599bdc63
vldktsn <vlad.K2001@gmail.com>
2021-05-29 20:50:18 +0300

    Another commit

---
+++ src/main/java/pja/edu/s19183/masmp5/DataInitializer.java
@@ -0,0 +1,25 @@
+package pja.edu.s19183.masmp5;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import pja.edu.s19183.masmp5.model.Company;
+import pja.edu.s19183.masmp5.repo.CompanyRepo;
+
+@Component
+@RequiredArgsConstructor
+public class DataInitializer {
+
+    private final CompanyRepo companyRepo;
+
+    @EventListener
+    public void atStart(ContextRefreshedEvent event){
+        System.out.println("Context refreshed");
+        Iterable<Company> all = companyRepo.findAll();
+        System.out.println(all);
+
+    }
+
+}
